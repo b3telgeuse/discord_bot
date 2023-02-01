36153019acbc3d3d44471f88155078a14719e000
Adam Ulrich <ulrichac00@gmail.com>
2023-02-01 13:02:08 -0500

    added comments

--- responses.py
+++ responses.py
@@ -4,25 +4,32 @@ player_hand = []
 dealer_hand = []
 player_total = 0
 dealer_total = 0
-player_record = {'wins': 0, 'losses': 0}
 player_balance = 200
-bet = 0
+player_record = {'wins': 0, 'losses': 0}
+
 
 def handle_response(message) -> str:
     global player_hand
     global dealer_hand
     global player_total
     global dealer_total
+    global player_balance
     global player_record
+
+#--------------------------------------------------------Hello-----------------------------------------------------
 
     #response to "hello grum"
     p_message = message.lower()
     if p_message == 'hello grum':
         return 'what do you want'
 
+#---------------------------------------------------------Roll------------------------------------------------------
+
     #rolls dice and returns a value 1-6
     if p_message == '!roll':
         return str(random.randint(1, 6))
+
+#------------------------------------------------------Blackjack-----------------------------------------------------
 
     #starts blackjack and deals a hand to player and grum
     if p_message == '!blackjack':
@@ -31,7 +38,7 @@ def handle_response(message) -> str:
         player_total = sum(player_hand)
         dealer_total = sum(dealer_hand)
 
-        return 'Blackjack! Your hand is {} for a total of {}. Type "hit" to hit or "stay" to stay.'.format(player_hand, player_total)
+        return 'Blackjack! Your hand is {} for a total of {}. Type "!hit" to hit or "!stay" to stay.'.format(player_hand, player_total)
 
     #hit deals another "card", updates player hand value, and tells you that you lost if you bust and updates the win/loss record
     if p_message == '!hit':
@@ -41,7 +48,7 @@ def handle_response(message) -> str:
             player_record['losses'] += 1
             return 'You bust with a total of {}. Your win-loss record is {}-{}'.format(player_total, player_record['wins'], player_record['losses'])
         else:
-            return 'You hit! Your new hand is {} for a total of {}. Type "hit" to hit or "stay" to stay.'.format(player_hand, player_total)
+            return 'You hit! Your new hand is {} for a total of {}. Type "!hit" to hit or "!stay" to stay.'.format(player_hand, player_total)
 
     #stay deals another card to the dealer, tells you if you win from a dealer bust, and updates win/loss record
     if p_message == '!stay':
@@ -61,3 +68,5 @@ def handle_response(message) -> str:
         else:
             player_record['wins'] += 1
             return 'You win with a total of {} to dealer\'s {}. Your win-loss record is {}-{}'.format(player_total, dealer_total, player_record['wins'], player_record['losses'])
+
+#--------------------------------------------------Blackjack with betting--------------------------------------------
